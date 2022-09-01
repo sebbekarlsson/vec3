8dc81475c02d85b0af59810b145d0e8c3954f8f7
sebastian karlsson <sebbekarlsson97@gmail.com>
2022-09-01 17:15:02 +0200

    triangle buffer

---
+++ include/vec3/triangle_buffer.h
@@ -0,0 +1,44 @@
+#ifndef VEC3_TRIANGLE_VEC3_TRIANGLE_BUFFER_H
+#define VEC3_TRIANGLE_VEC3_TRIANGLE_BUFFER_H
+#include <vec3/vec3.h>
+#include <vec3/matrix.h>
+#include <vec3/buffers.h>
+#include <vec3/triangle.h>
+
+typedef struct {
+  VEC3Triangle* items;
+  int64_t length;
+
+  unsigned int initialized;
+} VEC3TriangleBuffer;
+
+void vec3_triangle_buffer_init(VEC3TriangleBuffer* buffer);
+
+void vec3_triangle_buffer_deref(VEC3TriangleBuffer* buffer);
+
+void vec3_triangle_buffer_never_generate_normals(VEC3TriangleBuffer* buffer);
+
+int vec3_triangle_buffer_push(VEC3TriangleBuffer* buffer, VEC3Triangle v);
+
+unsigned int vec3_triangle_buffer_is_empty(VEC3TriangleBuffer buffer);
+int vec3_triangle_buffer_clear(VEC3TriangleBuffer* buffer);
+
+int vec3_triangle_buffer_allocate(VEC3TriangleBuffer* buffer, int64_t n);
+
+int vec3_triangle_buffer_translate(VEC3TriangleBuffer* buffer, VEC3Triangle pos, VEC3Rotation rotation);
+int vec3_triangle_buffer_translate_copy(VEC3TriangleBuffer* dest, VEC3TriangleBuffer src, VEC3Triangle pos, VEC3Rotation rotation);
+
+
+int vec3_triangle_buffer_pop(VEC3TriangleBuffer* buffer);
+
+VEC3Triangle vec3_triangle_buffer_back(VEC3TriangleBuffer buffer);
+
+VEC3Triangle vec3_triangle_buffer_get(VEC3TriangleBuffer buffer, int64_t index);
+int vec3_triangle_buffer_set(VEC3TriangleBuffer* buffer, int64_t index, VEC3Triangle v);
+
+
+int vec3_triangle_buffer_push_at(VEC3TriangleBuffer* a, VEC3Triangle v, int64_t index);
+int vec3_triangle_buffer_remove(VEC3TriangleBuffer* src, VEC3Triangle v);
+
+
+#endif
---
+++ src/triangle_buffer.c
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include <string.h>
+#include <vec3/macros.h>
+#include <vec3/triangle_buffer.h>
+
+void vec3_triangle_buffer_init(VEC3TriangleBuffer *buffer) {
+  if (buffer->initialized)
+    return;
+  buffer->items = 0;
+  buffer->length = 0;
+  buffer->initialized = 1;
+}
+
+#define VECTOR_BUFFER_GUARD(buffer)                                            \
+  {                                                                            \
+    if (!buffer->initialized) {                                                \
+      fprintf(stderr, "This vector buffer is not initialized! (%s)\n",         \
+              __func__);                                                       \
+    }                                                                          \
+  }
+
+int vec3_triangle_buffer_push(VEC3TriangleBuffer *buffer, VEC3Triangle v) {
+  VECTOR_BUFFER_GUARD(buffer);
+  buffer->items =
+      (VEC3Triangle *)realloc(buffer->items, (buffer->length + 1) * sizeof(VEC3Triangle));
+  buffer->items[buffer->length] = v;
+  buffer->length++;
+
+  return buffer->length > 0 && buffer->items != 0;
+}
+
+unsigned int vec3_triangle_buffer_is_empty(VEC3TriangleBuffer buffer) {
+  return buffer.length <= 0 || buffer.items == 0;
+}
+int vec3_triangle_buffer_clear(VEC3TriangleBuffer *buffer) {
+  VECTOR_BUFFER_GUARD(buffer);
+
+
+  if (!vec3_triangle_buffer_is_empty(*buffer)) {
+    free(buffer->items);
+    buffer->items = 0;
+    buffer->length = 0;
+  }
+
+  return 1;
+}
+
+int vec3_triangle_buffer_allocate(VEC3TriangleBuffer *buffer, int64_t n) {
+  VECTOR_BUFFER_GUARD(buffer);
+  buffer->items =
+      (VEC3Triangle *)realloc(buffer->items, (buffer->length + n) * sizeof(VEC3Triangle));
+  return buffer->items != 0;
+}
+
+
+int vec3_triangle_buffer_shift_left(VEC3TriangleBuffer *array, int index) {
+  if (!array || array->length <= 0) return 0;
+  for (int i = index; i < array->length - 1; i++)
+    array->items[i] = array->items[i + 1];
+
+  return 1;
+}
+
+int vec3_triangle_buffer_shift_right(VEC3TriangleBuffer *array, int index) {
+  if (!array || array->length <= 0) return 0;
+  for (int i = array->length - 1; i >= index; i--) {
+    array->items[MIN(array->length - 1, i + 1)] = array->items[i];
+    array->items[i] = (VEC3Triangle){0};
+  }
+
+  return 1;
+}
+
+int vec3_triangle_buffer_push_at(VEC3TriangleBuffer* a, VEC3Triangle v, int64_t index) {
+  if (!a) return 0;
+  a->items =
+    (VEC3Triangle *)realloc(a->items, (a->length + 1) * sizeof(VEC3Triangle));
+
+  a->length++;
+  vec3_triangle_buffer_shift_right(a, index);
+
+  a->items[index] = v;
+
+  return 1;
+}
+
+int vec3_triangle_buffer_remove_by_index(VEC3TriangleBuffer* src, int64_t index) {
+  if (!src || src->length <= 0) return 0;
+
+  if (index < 0) return 0;
+
+  vec3_triangle_buffer_shift_left(src, index);
+
+  if (src->length-1 <= 0) {
+    vec3_triangle_buffer_clear(src);
+    return 1;
+  }
+
+  src->items = (VEC3Triangle*)realloc(src->items, (src->length-1) * sizeof(VEC3Triangle));
+  src->length -= 1;
+
+  return 1;
+}
+
+
+int vec3_triangle_buffer_pop(VEC3TriangleBuffer* buffer) {
+  if (buffer->length <= 0 || buffer->items == 0) return 0;
+
+  if (buffer->length-1 <= 0) {
+    vec3_triangle_buffer_clear(buffer);
+    return 1;
+  }
+
+ // buffer->items[buffer->length-1] = 0;
+
+
+  buffer->items = (VEC3Triangle*)realloc(buffer->items, (buffer->length-1) * sizeof(VEC3Triangle));
+  buffer->length -= 1;
+  return 1;
+}
